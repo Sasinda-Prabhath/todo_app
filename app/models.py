7a2cea492bd8e2545c950c9b4678148a726feb52
Sasinda Prabhath <sasindaprabhath94@gmail.com>
2025-08-11 20:49:10 +0530

    Trigger pipeline run

--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
 from . import db
 
+
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(200), nullable=False)
--- app/routes.py
+++ app/routes.py
@@ -2,12 +2,15 @@ from flask import Blueprint, render_template, request, redirect, url_for
 from . import db
 from .models import Todo
 
+
 todo_bp = Blueprint('todo_bp', __name__, template_folder='templates')
+
 
 @todo_bp.route('/')
 def index():
     todos = Todo.query.all()
     return render_template('index.html', todos=todos)
+
 
 @todo_bp.route('/add', methods=['POST'])
 def add():
@@ -17,6 +20,7 @@ def add():
         db.session.add(new_task)
         db.session.commit()
     return redirect(url_for('todo_bp.index'))
+
 
 @todo_bp.route('/delete/<int:id>')
 def delete(id):
